3e95873463d4702dfc12c28fc563f614cd036268
bruno <bruno.marqs11@gmail.com.br>
2024-04-29 22:30:07 -0300

    Alterações testes java

--- meu-primeiro-projeto-java/src/App.java
+++ meu-primeiro-projeto-java/src/App.java
@@ -1,5 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Salve, salve!");
+        int modulo = 18 % 3;
+
+        int a,b;
+
+        a = 5;
+        b = 5;
+
+        String resultado = a==b ?"Verdadeiro" : "Falso";
+
+        System.out.println(resultado);
     }
 }

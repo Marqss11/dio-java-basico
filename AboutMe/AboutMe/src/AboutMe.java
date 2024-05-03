054b0b27eb8ef06b962789c2fdf9d0978bc31902
bruno <bruno.marqs11@gmail.com.br>
2024-05-02 21:23:59 -0300

    Commit AboutMe

---
+++ AboutMe/AboutMe/src/AboutMe.java
@@ -0,0 +1,30 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AboutMe {
+    public static void main(String[] args) {
+        //criando o objeto scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        
+        System.out.println("Informe seu nome");
+        String nome = scanner.next();
+        
+        System.out.println("Informe seu sobrenome");
+        String sobrenome = scanner.next();
+
+        System.out.println("Informe sua idade");
+        int idade = scanner.nextInt();
+        
+        
+        System.out.println("Informe sua altura");
+        double altura = scanner.nextDouble();
+
+        
+        //imprimindo os dados obtidos pelo usuario
+        System.out.println("Olá, me chamo " + nome + " " + sobrenome);
+        System.out.println("Tenho " + idade + " anos ");
+        System.out.println("Minha altura é " + altura + "cm ");
+        
+        
+    }
+}
